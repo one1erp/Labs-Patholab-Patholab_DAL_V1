8c30e9c859bf6ef6fc1511ffbf620b4491140993
one1erp <avigaile@one1.co.il>
2024-05-01 16:21:30 +0300

    aaa

---
+++ Patholab_DAL_V1/Packages/DbTracingListener.cs
@@ -0,0 +1,41 @@
+﻿
+
+//using Clutch.Diagnostics.EntityFramework;
+//using System;
+//using System.Data.Common;
+//using System.Diagnostics;
+
+//namespace Patholab_DAL_V1.Packages
+//{
+
+//    public class DbTracingListener : IDbTracingListener
+//    {
+
+
+//        public void CommandExecuting(DbTracingContext context)
+//        {
+//            //    throw new NotImplementedException();
+//        }
+
+//        public void CommandFinished(DbTracingContext context)
+//        {
+//            //     throw new NotImplementedException();
+//        }
+
+//        public void ReaderFinished(DbTracingContext context)
+//        {
+//            //    throw new NotImplementedException();
+//        }
+
+//        public void CommandFailed(DbTracingContext context)
+//        {
+//            Patholab_Common.Logger.WriteQueries("Command Failed " + context.Command.CommandText + " result - " + context.Result);
+//        }
+
+//        public void CommandExecuted(DbTracingContext context)
+//        {
+//            Patholab_Common.Logger.WriteQueries(context.Command.CommandText + "Executed in: " + context.Duration + "\n" + " Finish time - " + (context.FinishTime.HasValue ? "" : context.FinishTime.Value.ToString("yyyy/MM/dd/HH:mm:ss.FFF")));
+
+//        }
+//    }
+//}
---
+++ Patholab_DAL_V1/Packages/RunFunction.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using OracleCommand = Oracle.DataAccess.Client.OracleCommand;
+//using OracleConnection = Oracle.DataAccess.Client.OracleConnection;
+using OracleCommand = Oracle.ManagedDataAccess.Client.OracleCommand;
+using OracleConnection = Oracle.ManagedDataAccess.Client.OracleConnection;
+using Patholab_Common;
+
+namespace Patholab_DAL_V1.Packages
+{
+    public class RunFunction
+    {
+        internal static DateTime RunGetSysdate(Entities context)
+        {
+            var res = context.Database.SqlQuery<DateTime>(" select  sysdate from dual ");
+            return res.SingleOrDefault();
+        }
+
+        internal static long Run_Authorization_signed_by(Entities context, long sdgId, long index)
+        {
+            var res = context.Database.SqlQuery<long>(" select  LIMS.Authorization.signed_by(" + sdgId + "," + index + ") from dual ");
+            return res.SingleOrDefault();
+        }
+
+        internal static decimal? Run_Get_Price(Entities context, long p_customer_id, long p_parts_id)
+        {
+            try
+            {
+                var res = context.Database.SqlQuery<decimal?>(" select  LIMS.Get_Price(" + p_customer_id + "," + p_parts_id + ") from dual ");
+                if (res.SingleOrDefault() == null)
+                    return null;
+                else
+                    return res.SingleOrDefault();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        internal static decimal? Run_Get_Price_Urgent(Entities context, long p_customer_id, long p_parts_id)
+        {
+            try
+            {
+                var res = context.Database.SqlQuery<decimal?>(" select  LIMS.Get_Price_Urgent(" + p_customer_id + "," + p_parts_id + ") from dual ");
+                if (res.SingleOrDefault() == null)
+                    return null;
+                else
+                    return res.SingleOrDefault();
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        internal static bool Run_CheckId(Entities context, string id)
+        {
+            //Debugger.Launch();
+            if (id == null)
+            {
+                return false;
+            }
+
+            string sql = "select LIMS.check_id_Test ('" + id + "') from dual";
+            var res =
+                context.Database.SqlQuery<string>(sql);
+            //return res.SingleOrDefault() != null && res.Single() != null;
+            return res.SingleOrDefault() != null && res.Single() == "TRUE";
+
+
+            return false;
+        }
+        internal static string OTHER_SDG_FOR_PATIENT(Entities context, string id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            string sql = "select LIMS.OTHER_SDG_FOR_PATIENT ('" + id + "') from dual";
+            var res =
+                context.Database.SqlQuery<string>(sql);
+            return res.SingleOrDefault();
+
+
+            return null;
+        }
+
+        internal static decimal Run_PATHOLAB_SYNTAX(Entities context, long customerId, string dpt)
+        {
+
+            if (dpt.Length > 1)
+            {
+                return 0;
+            }
+            try
+            {
+                string sql = "select LIMS.PATHOLAB_SYNTAX (" + customerId + ",'" + dpt + "') from dual ";
+                var res =
+                    context.Database.SqlQuery<decimal>(sql);
+                return res.SingleOrDefault();
+            }
+            catch (Exception e)
+            {
+
+                return 0;
+
+            }
+        }
+
+        internal static decimal GetNewId(Entities context, string sequenceName)
+        {
+            try
+            {
+
+                string sql = "select lims." + sequenceName + ".nextval from dual";
+                var res =
+              context.Database.SqlQuery<decimal>(sql);
+                return res.SingleOrDefault();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                throw ex;
+
+            }
+        }
+        internal static bool OK_PAP_RESULT(Entities context, long sdgId)
+        {
+            string sql = "";
+            try
+            {
+                 sql = "begin lims.OK_PAP_RESULT  (" + sdgId.ToString() + " ); end;";
+                 var res =
+                     context.Database.ExecuteSqlCommand(sql);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.WriteLogFile(e);
+                Logger.WriteQueries(sql);
+                return false;
+            }
+        }
+        internal static bool Run_Insert_To_Sdg_Log(Entities context, long SdgId, string applicationCode, long sessionId, string Description)
+        {
+            // return false; //Works only for lims_sys
+            string sql = "";
+            // Debugger.Launch();
+            try
+            {
+                sql = "begin lims.Insert_To_Sdg_Log  (" + SdgId.ToString() + ", '" + applicationCode + "'," + sessionId.ToString() + ", '" + Description + "' ); end;";
+                var res =
+                    context.Database.ExecuteSqlCommand(sql);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Logger.WriteLogFile(e);
+                Logger.WriteQueries(sql);
+                return false;
+            }
+        }
+
+        internal static dynamic RunQuery(Entities context, string query)
+        {
+            try
+            {
+
+                return context.Database.SqlQuery<dynamic>(query);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        internal static IEnumerable<string> GetDynamicList(Entities context, string query)
+        {
+            try
+            {
+
+                var q = context.Database.SqlQuery<string>(query);
+                return q;
+            }
+            catch (Exception ex)
+            {
+                return null;
+
+            }
+        }
+        internal static string GetDynamicStr(Entities context, string sql)
+        {
+            try
+            {
+
+                var q = context.Database.SqlQuery<string>(sql);
+                return q.First();
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex.Message);
+                return null;
+
+            }
+        }
+        internal static decimal? GetDynamicDecimal(Entities context, string sql)
+        {
+            try
+            {
+
+                var q = context.Database.SqlQuery<decimal>(sql);
+                return q.First();
+            }
+            catch (Exception ex)
+            {
+                return null;
+
+            }
+        }
+        internal static string ExecuteScalar(Entities context, string query)
+        {
+            try
+            {
+
+                string res = context.Database.SqlQuery<string>(query).FirstOrDefault();
+                return res;
+
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        internal static List<ObjDetails> GetObjDetailses(Entities context, string tableName, string condition)
+        {
+            try
+            {
+
+
+                if (!string.IsNullOrEmpty(condition))
+                {
+                    condition = "where " + condition;
+                }
+                var q = context.Database.SqlQuery<ObjDetails>("Select " + tableName + "_Id as ID,NAME from lims_sys." + tableName + "  " + condition).ToList();
+                return q;
+
+            }
+            catch (Exception e)
+            {
+                Logger.WriteLogFile(e);
+                return null;
+            }
+
+        }
+
+        internal static decimal GetSamplesByContainer(Entities context, decimal containerId)
+        {
+            return GetSamplesByContainer(context, containerId.ToString());
+        }
+
+
+        internal static decimal GetSdgByContainer(Entities context, string containerId)
+        {
+            try
+            {
+
+
+
+                string sql = "select count(1) from lims_sys.sdg_user where u_container_id='" + containerId + "'";
+                var res =
+              context.Database.SqlQuery<decimal>(sql);
+                return res.SingleOrDefault();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                throw ex;
+
+            }
+        }
+        internal static decimal GetSamplesByContainer(Entities context, string containerId)
+        {
+            try
+            {
+
+                string sql = "select count(1) from lims_sys.sample where sdg_id in" +
+                    " (select sdg_id   from lims_sys.sdg_user where u_container_id='" + containerId + "')";
+                var res =
+              context.Database.SqlQuery<decimal>(sql);
+                return res.SingleOrDefault();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                throw ex;
+
+            }
+        }
+
+        internal static decimal GetSamplesByContainerAndStatus(Entities context, string containerId)
+        {
+            try
+            {
+
+                string sql = "select count(1) from lims_sys.sample where sdg_id in" +
+                    " (select sdg_id   from lims_sys.sdg_user where u_container_id='" + containerId + "') and status <> 'U'";
+                var res =
+              context.Database.SqlQuery<decimal>(sql);
+                return res.SingleOrDefault();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                throw ex;
+
+            }
+        }
+
+        private static dynamic GetDataRow(DbDataReader dataReader)
+        {
+            var dataRow = new ExpandoObject() as IDictionary<string, object>;
+            for (var fieldCount = 0; fieldCount < dataReader.FieldCount; fieldCount++)
+                dataRow.Add(dataReader.GetName(fieldCount), dataReader[fieldCount]);
+            return dataRow;
+        }
+
+        internal static decimal IfExitEXTRA_SLIDES_STATUS_P(Entities context, string BlockNumber, string ExRequestCreatedOn)
+        {
+            try
+            {
+
+                string sql = "select count(1) from lims.EXTRA_SLIDES_STATUS_P where BlockNumber ='" + BlockNumber + "' and ExRequestCreatedOn = '" + ExRequestCreatedOn + "'";
+                var res =
+              context.Database.SqlQuery<decimal>(sql);
+                return res.SingleOrDefault();
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLogFile(ex);
+                throw ex;
+
+            }
+        }
+    }
+}
